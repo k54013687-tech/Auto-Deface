f7dfea18a2807ff58b99d05f5304edca3c65db7c
JUNED-404 <k54013687@gmail.com>
2026-02-18 18:59:33 +0700

    Add files via upload

---
+++ Auto_Depes.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+# apa lu mau dec😂
+# ================================================
+# kang dec njir 😹
+# ================================================
+# rename sc orng malu dek😝
+# ================================================
+# ]=)}#<&/<<]/]+$2#:)-)>^$<}~>-:$>@-*>#}[({[{();-:)2~_^-2>~=}^
+# #&;<<_*-<#:^];>@)&+^]#~+2=#@[&[=:[(:_)&-$^:<(#(2{}_(2}+${+)>
+# _#)-2_>:@^+@)(:>;:=>~^;&>=(><*~])}<<(=(=]_=~^=;_<}}{#-><;])*
+# ================================================
+# apa lu mau dec😂
+# ================================================
+# kang dec njir 😹
+# ================================================
+# rename sc orng malu dek😝
+# ================================================
+# apa lu mau dec 😂
+# ================================================
+# kang dec njir 😹
+# ================================================
+# rename sc orng malu dek😝
+# ================================================
+# apa lu mau dec 😂
+# ================================================
+# kang dec njir 😹
+# ================================================
+#=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+
+# KODE ENCRYPTED - JANGAN DIEDIT
+TMP_FILE=$(mktemp)
+cat > "$TMP_FILE" << "ENDOFFILE"
+IyEvdXNyL2Jpbi9CYXNoCgpCTEM9J1wwMzNbMTszMG0nCkJHPSdcMDMzWzQxbScKUFVUPSdcMDMzWzk3bScKTj0nXDAzM1swbScKUj0nXDAzM1swOzMxbScKRz0nXDAzM1swOzMybScKTz0nXDAzM1swOzMzbScKUD0iXDAzM1sybSIKQkw9J1wwMzNbMDs5Nm0nCkI9J1wwMzNbMTszNm0nCkJSPSJcMDMzWzA7MzRtIgpVPSJcMDMzWzA7MzVtIgp0aW1lX291dD0iMzAiCgp0cmFwICdlY2hvICJnYWJvbGVoIGtlbHVhciBsb2ggeWEiOycgU0lHSU5UIFNJR1RTVFAgU0lHVEVSTQoKd2hpbGUgVHJ1ZTsgZG8KICAgIGVjaG8gLWUgImdhYmlzYSBDVFJMIHdvayIKICAgIHNsZWVwIDIKZG9uZQoKY2xlYXIKZWNobyAtZSAiICAgICAgICAgICAgICAgICAgICAgICAgXDAzM1s3OzM2bUhhbGxvIEJyb28ke059CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC8KICAgICAgICAg4paR4paR4paR4paR4paR4paR4paR4paE4paI4paE4paE4paE4paI4paEICAvICAgICAgICAgICR7Tn1cMDMzWzM7MzVCeSBUb29scyBVYmFoIFRlbWEganVuZWQke059CiAgICAgICAgIOKWhOKWgOKWkeKWkeKWkeKWkeKWhOKWjOKUgOKWhOKUgOKWhOKUgOKWkOKWhCDilpHilpHilpHiloDiloQgICAke1J9QWdyZXNzaXYke059CiAgICAgICAgIOKWiOKWhOKWhOKWiOKWkeKWkeKWgOKWjOKUgOKWgOKUgOKWgOKUgOKWkOKWgOKWkeKWkeKWiOKWhOKWhOKWiCAgICR7T30uL0p1bmVkJHtPfTMke099biR7Tn0gCiAgICAgICAgIOKWkeKWkOKWjOKWkeKWkeKWkeKWkeKWgOKWgOKWiOKWiOKWiOKWgOKWgOKWkeKWkeKWkeKWkeKWkOKWjAogICAgICAgICDilojilojilojilojilpHiloTilojilojilojilojilojilojilojilojilojiloTilpHilojilojilojilogKICAgICAgICAgIgogICAgICAgICAKZWNobyAtZSAiJHtCR30ke0JMQ31CRU5UQVIgQ0VLIE1PRFVMRSBEVUxVIFdPSyR7Tn0iCnNsZWVwIDIKCmNsZWFyCiM9PT09SW5zdGFsbCBNb2R1bGU9PT09CnBpcCBpbnN0YWxsIHN5cwpwaXAgaW5zdGFsbCByZXF1ZXN0cwpwaXAgaW5zdGFsbCB0ZWxlYm90CnBpcCBpbnN0YWxsIHN1YnByb2Nlc3MKCmNsZWFyCnNsZWVwIDEKcHl0aG9uMyBtYWlubm4ucHkK
+ENDOFFILE
+base64 -d "$TMP_FILE" | bash
+rm -f "$TMP_FILE"
